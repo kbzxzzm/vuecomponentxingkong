2ffdd8b423e23a0731d84ed7bff0e4e011483c95
kbzxzzm <508153455@qq.com>
2020-02-11 18:09:00 +0800

    '星空'

---
+++ src/components/xingkong.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="bg">
+      <!-- color: String类型。默认'#dedede'。粒子颜色。
+        particleOpacity: Number类型。默认0.7。粒子透明度。
+        particlesNumber: Number类型。默认80。粒子数量。
+        shapeType: String类型。默认'circle'。可用的粒子外观类型有："circle","edge","triangle", "polygon","star"。
+        particleSize: Number类型。默认80。单个粒子大小。
+        linesColor: String类型。默认'#dedede'。线条颜色。
+        linesWidth: Number类型。默认1。线条宽度。
+        lineLinked: 布尔类型。默认true。连接线是否可用。
+        lineOpacity: Number类型。默认0.4。线条透明度。
+        linesDistance: Number类型。默认150。线条距离。
+        moveSpeed: Number类型。默认3。粒子运动速度。
+        hoverEffect: 布尔类型。默认true。是否有hover特效。
+        hoverMode: String类型。默认true。可用的hover模式有: "grab", "repulse", "bubble"。
+        clickEffect: 布尔类型。默认true。是否有click特效。
+        clickMode: String类型。默认true。可用的click模式有: "push", "remove", "repulse", "bubble" -->
+    <vue-particles
+      color="#dedede"
+      :particleOpacity="0.7"
+      :particlesNumber="80"
+      shapeType="star"
+      :particleSize="4"
+      linesColor="#FFFFFF"
+      :linesWidth="2"
+      :lineLinked="true"
+      :lineOpacity="0.4"
+      :linesDistance="150"
+      :moveSpeed="3"
+      :hoverEffect="true"
+      hoverMode="grab"
+      :clickEffect="true"
+      clickMode="push"
+    />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#bg {
+  background-color: black;
+  overflow: hidden;
+  width: 100%;
+  height:698px
+}
+* {
+  padding: 0;
+  margin: 0;
+}
+</style>
+
